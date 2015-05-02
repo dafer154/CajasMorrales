51bb9593c10ca71c02d0e7d13471aa8469943410
andres1521 <roandres1521@gmail.com>
2015-05-02 16:53:37 -0500

    cambion en GUI

--- Morrales/src/morrales/UI/SubirArchivoGUI.java
+++ Morrales/src/morrales/UI/SubirArchivoGUI.java
@@ -89,18 +89,14 @@ public class SubirArchivoGUI extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
         ResolverProblema resolver;
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
         fl_buscarRuta.showOpenDialog(this);
-        resolver= new ResolverProblema(fl_buscarRuta.getSelectedFile().getPath());
-        
 // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void bt_calcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_calcularActionPerformed
-
-        ta_resultado.setText( resolver.resolver());
-
-// TODO add your handling code here:
+        if (fl_buscarRuta.getSelectedFile()!=null)
+        {resolver= new ResolverProblema(fl_buscarRuta.getSelectedFile().getPath());
+        ta_resultado.setText( resolver.resolver());}
     }//GEN-LAST:event_bt_calcularActionPerformed
 
 
